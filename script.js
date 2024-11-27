d0a71cc1a20e322eb1a32884ad8c06a7c4d428c8
Naeem Khan <khannaeem05563@gmail.com>
2024-11-27 12:20:06 +0530

    Third JS

---
+++ script.js
@@ -0,0 +1,47 @@
+// Reguler function 
+
+function display(){
+    alert("Function called");
+};
+
+/*// function expresion 
+
+const display1 = function(){
+    alert("function expresion called");
+};
+
+// arrow function 
+
+const display2 = () => {
+    alert("Arrow function called");
+};
+
+let button = document.getElementById("btn");
+
+// Whenever you pass a function as an argument to another function ---> never use -->().
+
+button.addEventListener("click", display);
+
+function display(){
+    alert("function called for function");
+}
+
+// You can call reguler function before declaration but not arrow function and fuction expression
+
+ button.addEventListener("click", display1);
+ button.addEventListener("mouseover", function(){
+    console.log("Hello From MouseOver Event")
+ });
+
+
+
+// querry selector
+
+let h1 = document.querySelectorAll("h1");
+console.log(h1);
+
+h1[0].innerHTML = "Hello from Querry selector";
+h1[0].style.color = "red";
+h1[0].style.backgroundColor = "pink";
+h1[0].style.padding = "20px";
+h1[0].style.boxShadow = "4px 4px 8px gray";*/
